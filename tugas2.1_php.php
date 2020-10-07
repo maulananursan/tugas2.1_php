6086554dd5c664d1b985f987de689c4662e65859
Maulana Nursan <71365575+maulananursan@users.noreply.github.com>
2020-10-07 11:58:01 +0700

    Add files via upload

---
+++ tugas2.1_php.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+	$h1=36;
+	$pk1=12;
+	$pk2=24;
+
+	$h2=($h1*$pk1)/$pk2;
+
+	?>
+	<p>36 Hari = 12 Pekerja</p>
+	<p>X hari = 24 Pekerja</p>
+	<p>X = ...?</p>
+	<p>Jawaban : <?php echo $h2; ?></p>
+
+</body>
+</html>
